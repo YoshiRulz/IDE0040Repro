bcf5de7e0c34835c1d90aaf44900c2f16aa3ad06
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-09-08 17:19:42 +1000

    Fix code style

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-/*internal*/ class AClass {} // correctly flagged
+internal class AClass {} // correctly not flagged
 public interface AnInterface { // correctly not flagged
 	/*public*/ int Number { get; } // correctly not flagged
-	public string String { get; } // correctly flagged
+	/*public*/ string String { get; } // correctly not flagged
 	public struct AStruct {} // erroneously flagged
 }
