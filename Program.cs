7f64e8ee4c990df5a569e2e009b7685212ee9e5f
YoshiRulz <OSSYoshiRulz+git@gmail.com>
2025-09-08 16:51:59 +1000

    Init

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*internal*/ class AClass {}
+public interface AnInterface {
+	/*public*/ int Number { get; }
+	public string String { get; }
+	public struct AStruct {}
+}
